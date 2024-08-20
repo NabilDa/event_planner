0de688296c943c845f452b4ef856f211cdb846fd
NabilDa <naabil.daoui@gmail.com>
2024-08-20 07:12:07 -0400

    Create Footer.jsx and add it to EventPlanner.jsx as a child component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>&copy; Event Planner Organization. All rightes reserved.</p>
+            </footer>
+        </>
+    )
+}
+export default Footer;
